51d51fd771f09c5aba853bac09af2ad9a4248fa8
SM <123@gmail.com>
2022-07-06 04:26:49 +0600

    home page activity done

--- app/src/main/java/com/example/androidproject/Update_Activity.java
+++ app/src/main/java/com/example/androidproject/Update_Activity.java
@@ -2,13 +2,39 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+//adding activity basically!!
 
 public class Update_Activity extends AppCompatActivity {
 
+    Button button1,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        button1=findViewById(R.id.button);
+        button2=findViewById(R.id.button2);
+        /**
+         * button1 represents ADD button
+         */
+        button1.setOnClickListener(view -> {
+            startActivity(new Intent(Update_Activity.this,AddingInfo.class));
+        });
+
+        /**
+         * button2 represents VIEW button
+         */
+
+        button2.setOnClickListener(view -> {
+            startActivity(new Intent(Update_Activity.this,ViewInfo.class));
+        });
+
+
+
     }
 }
